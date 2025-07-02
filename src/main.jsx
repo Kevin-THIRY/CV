13a2a41c7cd080afc056155b273f6ff8425b1774
Kevin THIRY <kevin.damien@hotmail.fr>
2025-07-02 19:43:51 +0200

    init cv

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import CV from './CV'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CV />
+  </React.StrictMode>,
+)
